c8799c576b34a434fa1514d03115b3703bfed02e
ishikawa999 <remember929@gmail.com>
2025-02-27 08:15:39 +0000

    Add space

--- test/system/copy_pre_content_to_clipboard_test.rb
+++ test/system/copy_pre_content_to_clipboard_test.rb
@@ -15,73 +15,94 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 require_relative '../application_system_test_case'
+
 class CopyPreContentToClipboardSystemTest < ApplicationSystemTestCase
   def test_copy_issue_pre_content_to_clipboard_if_common_mark
     log_user('jsmith', 'jsmith')
     issue = Issue.find(1)
     issue.journals.first.update(notes: "```\ntest\n```")
     visit "/issues/#{issue.id}"
+
     # A button appears when hovering over the <pre> tag
     find("#journal-#{issue.journals.first.id}-notes div.pre-wrapper:first-of-type").hover
     find("#journal-#{issue.journals.first.id}-notes div.pre-wrapper:first-of-type .copy-pre-content-link")
+
     # Copy pre content to Clipboard
     find("#journal-#{issue.journals.first.id}-notes div.pre-wrapper:first-of-type .copy-pre-content-link").click
+
     # Paste the value copied to the clipboard into the textarea to get and test
     first('.icon-edit').click
     find('textarea#issue_notes').send_keys([modifier_key, 'v'])
     assert_equal find('textarea#issue_notes').value, 'test'
   end
+
   def test_copy_issue_code_content_to_clipboard_if_common_mark
     log_user('jsmith', 'jsmith')
     issue = Issue.find(1)
     issue.journals.first.update(notes: "```ruby\nputs \"Hello, World.\"\n```")
     visit "/issues/#{issue.id}"
+
     # A button appears when hovering over the <pre> tag
     find("#journal-#{issue.journals.first.id}-notes div.pre-wrapper:first-of-type").hover
     find("#journal-#{issue.journals.first.id}-notes div.pre-wrapper:first-of-type .copy-pre-content-link")
+
     # Copy pre content to Clipboard
     find("#journal-#{issue.journals.first.id}-notes div.pre-wrapper:first-of-type .copy-pre-content-link").click
+
     # Paste the value copied to the clipboard into the textarea to get and test
     first('.icon-edit').click
     find('textarea#issue_notes').send_keys([modifier_key, 'v'])
     assert_equal find('textarea#issue_notes').value, 'puts "Hello, World."'
   end
+
   def test_copy_issue_pre_content_to_clipboard_if_textile
     log_user('jsmith', 'jsmith')
     issue = Issue.find(1)
     issue.journals.first.update(notes: "<pre>\ntest\n</pre>")
+
     with_settings text_formatting: :textile do
       visit "/issues/#{issue.id}"
+
       # A button appears when hovering over the <pre> tag
       find("#journal-#{issue.journals.first.id}-notes div.pre-wrapper:first-of-type").hover
       find("#journal-#{issue.journals.first.id}-notes div.pre-wrapper:first-of-type .copy-pre-content-link")
+
       # Copy pre content to Clipboard
       find("#journal-#{issue.journals.first.id}-notes div.pre-wrapper:first-of-type .copy-pre-content-link").click
+
       # Paste the value copied to the clipboard into the textarea to get and test
       first('.icon-edit').click
       find('textarea#issue_notes').send_keys([modifier_key, 'v'])
       assert_equal find('textarea#issue_notes').value, 'test'
     end
   end
+
   def test_copy_issue_code_content_to_clipboard_if_textile
     log_user('jsmith', 'jsmith')
     issue = Issue.find(1)
     issue.journals.first.update(notes: "<pre><code class=\"ruby\">\nputs \"Hello, World.\"\n</code></pre>")
+
     with_settings text_formatting: :textile do
       visit "/issues/#{issue.id}"
+
       # A button appears when hovering over the <pre> tag
       find("#journal-#{issue.journals.first.id}-notes div.pre-wrapper:first-of-type").hover
       find("#journal-#{issue.journals.first.id}-notes div.pre-wrapper:first-of-type .copy-pre-content-link")
+
       # Copy pre content to Clipboard
       find("#journal-#{issue.journals.first.id}-notes div.pre-wrapper:first-of-type .copy-pre-content-link").click
+
       # Paste the value copied to the clipboard into the textarea to get and test
       first('.icon-edit').click
       find('textarea#issue_notes').send_keys([modifier_key, 'v'])
       assert_equal find('textarea#issue_notes').value, 'puts "Hello, World."'
     end
   end
+
   private
+
   def modifier_key
     modifier = osx? ? 'command' : 'control'
     modifier.to_sym
