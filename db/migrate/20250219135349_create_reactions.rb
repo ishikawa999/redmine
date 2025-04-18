ad383b8b10fdf007f1374e19cda9481c4043c895
Katsuya HIDAKA <hidakatsuya@gmail.com>
2025-04-18 13:25:24 +0900

    Enforce non-null constraints on reactions table references

--- db/migrate/20250219135349_create_reactions.rb
+++ db/migrate/20250219135349_create_reactions.rb
@@ -1,8 +1,8 @@
 class CreateReactions < ActiveRecord::Migration[7.2]
   def change
     create_table :reactions do |t|
-      t.references :reactable, polymorphic: true
-      t.references :user, foreign_key: true
+      t.references :reactable, polymorphic: true, null: false
+      t.references :user, foreign_key: true, null: false
       t.timestamps null: false
     end
     add_index :reactions, [:reactable_type, :reactable_id, :user_id], unique: true
